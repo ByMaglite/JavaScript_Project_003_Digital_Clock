4cbbb8c457df358210c22aaadaa562fc6ad1446c
ByMaglite <fceyhan55@gmail.com>
2021-09-19 10:57:10 +0300

    completed

---
+++ app.js
@@ -0,0 +1,10 @@
+function showTime(){
+    const time = new Date();
+    const timeLabel = document.querySelector('p');
+
+    timeLabel.innerHTML = time.toLocaleTimeString(); 
+};
+
+const clock = setInterval(() => {
+    showTime();
+}, 1000);
